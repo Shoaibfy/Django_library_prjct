81a3660dee176130780da9ee6a64f43fc4a4a685
shoaibfy <shoaibfy@gmail.com>
2019-11-25 09:37:00 +0530

    Django_library_project

---
+++ library/libraryapp/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    author_name=models.CharField(max_length=100,help_text="enter author name")
+
+
+    def __str__(self):
+        return self.author_name
+
+
+class Category(models.Model):
+    category_name=models.CharField(max_length=100,help_text="enter Book category")
+
+
+    def __str__(self):
+        return self.category_name
+
+
+class Book(models.Model):
+    book_name=models.CharField(max_length=100,help_text="enter name of book")
+    book_price=models.IntegerField(help_text="enter price of book")
+    book_isbn_Num=models.CharField(max_length=100, help_text="enter isbn number")
+    author_name=models.ForeignKey(Author,on_delete = models.CASCADE)
+    category_name=models.ForeignKey(Category,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.book_name
+
+
+class Member(models.Model):
+     member_name=models.CharField(max_length=100,help_text="enter name of member")
+     member_last_name=models.CharField(max_length=100,help_text="enter last name of member")
+     member_phone_number=models.IntegerField(help_text="enter phone number of a  member")
+     member_email=models.EmailField(help_text="enter email  of member")
+
+     def __str__(self):
+         return self.member_name
+
+class Record(models.Model):
+    issue_date=models.DateField()
+    return_date=models.DateField()
+    book_name=models.ForeignKey(Book,on_delete=models.CASCADE)
+    member_name=models.ForeignKey(Member,on_delete=models.CASCADE)
+
+def __str__(self):
+    return self.issue_date
+
+
+
